d228590f5831e22293ee934d4d5b2f6d5e916c2a
CHAKMING <1766069334@qq.com>
2024-04-27 09:11:39 +0800

    commit all infomation

---
+++ lab2:system_calls/kernel/sysproc.c
@@ -0,0 +1,116 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "sysinfo.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  argint(0, &n);
+  if(n < 0)
+    n = 0;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+uint64 
+sys_trace(void)
+{
+    argint(0, &myproc()->trace_mask); // from p->trapframe->a0 to proc->trace_mas
+    return 0;
+}
+
+uint64 
+sys_sysinfo(void)
+{
+    // get struct sysinfo*
+    uint64 vir_address;
+    argaddr(0, &vir_address);
+    struct sysinfo info;
+    info.freemem = count_freemem();
+    info.nproc = count_procs();
+    // We need to copy data to a given virtual address. 
+    if(copyout(myproc()->pagetable, vir_address, (char*)&info, sizeof(struct sysinfo)) == -1)
+        return -1;
+    return 0;
+}
